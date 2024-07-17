3787400bf3e124ed8585f1d269e0d9d5c0530eb5
Jim Fleax <jimfleax@proton.me>
2024-07-17 22:48:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -505,7 +505,7 @@ CosmicMath = {
         }
 
         !(document.querySelector('#braincrunchmode')) ? (fluentModeEl = document.createElement('div'),
-        fluentModeEl.innerHTML = `<span title="Fluent Mode" onclick="$.score(0);CosmicMath.fluentMode('${input}', '${address}');$.msg('Fluent Mode is active');" id="braincrunchmode" style="visibility: visible;"><span style=" filter: contrast(0.01); ">⚡</span></span>`,
+        fluentModeEl.innerHTML = `<span title="Fluent Mode" onclick="$.score(0);CosmicMath.fluentMode('${input}', '${address}');$.msg('Fluent Mode is active');" id="braincrunchmode" style="visibility: visible;"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="fluent" viewBox="0 0 297.961 297.961" xml:space="preserve"> <path d="M235.443,4.316C234.064,1.675,231.331,0,228.35,0H111.391c-3.537,0-6.654,2.341-7.666,5.729l-41.78,140.009  c-0.723,2.422-0.259,5.029,1.251,7.055c1.51,2.027,3.889,3.207,6.415,3.207h65.579l-26.419,132.38  c-0.769,3.85,1.369,7.697,5.047,9.07c0.917,0.342,1.863,0.511,2.796,0.511c2.806,0,5.496-1.48,6.954-4.04l103.03-180.928  c1.41-2.477,1.396-5.533-0.036-7.997c-1.432-2.464-4.066-3.996-6.916-3.996h-46.392l61.658-88.415  C236.617,10.14,236.822,6.959,235.443,4.316z"></path> </svg></span></span>`,
         document.querySelector('body').append(fluentModeEl)) : null;
         $_ = Boolean(document.querySelector('#sc'));
         var helpcontent;
