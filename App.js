09a0fc2738ec346f7ccaa55f058e776a4c398b2d
Jim Fleax <jimfleax@proton.me>
2024-07-25 01:23:00 +0530

    Added new sums and mixed courses.

--- App.js
+++ App.js
@@ -266,12 +266,6 @@ CosmicMath = {
 
               document.querySelector("#mainBoard").append($$$);
               $$$.onclick = function () {
-                document.querySelector(
-                  "#app > svg"
-                ).innerHTML = `<path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"></path>`;
-                document.querySelector("#app > svg").onclick = () => {
-                  CosmicMath.launchApp();
-                };
                 $.mixedCourse();
               };
             }
@@ -291,6 +285,12 @@ CosmicMath = {
       return a.includes(localStorage["cm_address"]);
     });
     basket = basket[$.random(basket.length - 1, true)];
+    document.querySelector(
+        "#app > svg"
+      ).innerHTML = `<path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"></path>`;
+      document.querySelector("#app > svg").onclick = () => {
+        CosmicMath.launchApp();
+      };
     $.createSum(basket.split("_").pop(), basket);
   },
   launchApp: function () {
