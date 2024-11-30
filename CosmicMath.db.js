64c34e1dcdb3e5e0e1d5646827f94a58fc1343e7
Jim Fleax <jimfleax@proton.me>
2024-11-30 17:41:55 +0530

    New app - Coordinate Geometry > Polygon area

--- CosmicMath.db.js
+++ CosmicMath.db.js
@@ -3336,7 +3336,7 @@ const $db = {
 
       this.answer = area;
       return (
-        `If you join the points ${new Coordinates(points.pop()).toString() + " and " + new Coordinates(coordinates.points.slice(-1)[0]).toString()} to make a polygon, what will be the area?`
+        `If you join the points ${new Coordinates(points.pop()) + " and " + new Coordinates(coordinates.points.slice(-1)[0])} to make a polygon, what will be the area?`
       );
     },
     answer: null,
